3e663e107d335545a43fae1665e435ce8edd7554
Yaroslav-Kuznetsov <yaroslav.kuznetsov@devexpress.com>
2018-12-25 14:57:22 +0300

    T636808

--- CS/dxWinFormsSample/Form1.cs
+++ CS/dxWinFormsSample/Form1.cs
@@ -139,7 +139,13 @@ namespace dxWinFormsSample {
                 }
             }
         }
-        private void UnitComboBox_Closed(object sender, DevExpress.XtraEditors.Controls.ClosedEventArgs e) {
+
+        private void simpleButton1_Click(object sender, EventArgs e) {
+            new ReportDesignTool(new XtraReport()).ShowRibbonDesignerDialog();
+        }
+
+        private void UnitComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
             if ((sender as BaseEdit).EditValue != null && (sender as BaseEdit).EditValue is GraphicsUnit)
                 ConvertValues((GraphicsUnit)(sender as BaseEdit).EditValue,
                 LabelWidthSpinEdit.EditValue,
@@ -150,10 +156,6 @@ namespace dxWinFormsSample {
                 TopMarginSpinEdit.EditValue,
                 LeftMarginSpinEdit.EditValue,
                 RightMarginSpinEdit.EditValue);
-        }
-
-        private void simpleButton1_Click(object sender, EventArgs e) {
-            new ReportDesignTool(new XtraReport()).ShowRibbonDesignerDialog();
         }
     }
 }
