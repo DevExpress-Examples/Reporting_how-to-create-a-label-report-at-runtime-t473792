deff95ee6feebc65f5c4c78e96dd6b8baab43268
Polina Tyureva <polina.tyureva@devexpress.com>
2024-04-08 13:29:56 +0400

    add vb solution

---
+++ VB/Reporting_how-to-create-a-label-report-at-runtime-t473792/LabelReportRuntime/CustomLabelReportModel.vb
@@ -0,0 +1,23 @@
+﻿Namespace dxWinFormsSample
+    Public Class CustomLabelReportModel
+        Public Property LabelWidth() As Single
+        Public Property LabelHeight() As Single
+        Public Property VerticalPitch() As Single
+        Public Property HorizontalPitch() As Single
+        Private _Unit As DevExpress.Drawing.DXGraphicsUnit = DevExpress.Drawing.DXGraphicsUnit.Millimeter
+        Public Property MeasurementUnit() As DevExpress.Drawing.DXGraphicsUnit
+            Get
+                Return _Unit
+            End Get
+            Set(ByVal value As DevExpress.Drawing.DXGraphicsUnit)
+                _Unit = value
+            End Set
+        End Property
+        Public Property BottomMargin() As Single
+        Public Property TopMargin() As Single
+        Public Property LeftMargin() As Single
+        Public Property RightMargin() As Single
+        Public Property PaperKindID() As Integer
+
+    End Class
+End Namespace
